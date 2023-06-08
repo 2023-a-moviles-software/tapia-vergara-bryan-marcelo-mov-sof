188c0d8bebbc23bfc4e8195b1230311d99319874
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-06-08 10:26:18 -0500

    Creación de botón y acción

--- 01-android/app/src/main/java/com/example/moviles01/ACicloVida.kt
+++ 01-android/app/src/main/java/com/example/moviles01/ACicloVida.kt
@@ -3,21 +3,12 @@ package com.example.moviles01
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class ACicloVida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val botonCicloVida = findViewById<Button>(
-            R.id.btn_ciclo_vida
-        )
-        botonCicloVida.setOnClickListener {
-            irActividad(ACicloVida::class.java)
-        }
+        setContentView(R.layout.activity_aciclo_vida)
     }
-
     fun irActividad(
         clase: Class<*>
     ){
