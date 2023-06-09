a4f9f8b5f9b0714e79ee1bd108db462f3f0d7e30
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-06-08 19:07:45 -0500

    Mensaje en LAND

--- 01-android/app/src/main/java/com/example/moviles01/ACicloVida.kt
+++ 01-android/app/src/main/java/com/example/moviles01/ACicloVida.kt
@@ -1,10 +1,11 @@
 package com.example.moviles01
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ACicloVida : AppCompatActivity() {
+class ACicloVida : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
